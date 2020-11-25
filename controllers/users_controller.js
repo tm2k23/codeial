981923a704efe3828270014f7539200a961fd8d3
Tejas <tejasmishra2k18@gmail.com>
2020-11-25 11:30:39 +0530

    add user to database

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+const { response } = require('express');
+const User = require('../models/user.js'); // import the user model
+
 module.exports.profile = function(req, res) {
     return res.render('profile.ejs', {
         title: "Profile"
@@ -20,8 +23,26 @@ module.exports.signIn = function(req, res) {
 
 module.exports.create = function(req, res) {
     // to add user to the data base 
-    // todo later
-    return res.end('created');
+
+    if (req.body.password != req.body.confirm_password) // if the password does not match
+        return res.redirect('back'); // redirect back to the signup page
+
+    // check if the user email already exists
+    User.findOne({ email: req.body.email }, function(err, user) {
+        if (err) { console.log('Error in seraching the signup email in database ', err); } // if there is error
+
+        if (!user) {
+            // if user does not exist
+            User.create(req.body, function(err, user) {
+                if (err) { console.log('Error in creating new user ', err); }
+                console.log('new user created : ', user); // print the user data on console
+                return res.redirect('/user/sign-in'); // redirect to signin page when the user is created
+            });
+        } else {
+            console.log('User already exists');
+            return res.redirect('back');
+        }
+    });
 }
 
 module.exports.createSession = function(req, res) {
