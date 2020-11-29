2a5f84c3dd9a98a9f30aeff131a1b1321b26492a
Tejas <tejasmishra2k18@gmail.com>
2020-11-29 13:58:56 +0530

    add feature to delete a post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
+const { post } = require('../routes/posts');
 module.exports.create = function(req, res) {
     Post.create({
         content: req.body.content,
@@ -9,4 +11,25 @@ module.exports.create = function(req, res) {
         }
         return res.redirect('back');
     });
+}
+
+module.exports.destroy = function(req, res) {
+    // console.log('Request Params : ', req.params);
+    // first look if the post in the database esists or not 
+    Post.findById(req.params.postIdToDelete, function(err, post) {
+        if (post) {
+            // if the post exist , check if the post is deleted by the owner only 
+            // console.log('post.user : ', post.user);
+            // console.log(req.user);
+            // console.log(typeof(req.user._id), " ", typeof(req.user.id));
+            if (post.user == req.user.id) //if (post.user == req.user._id) why not this ?
+            { // tocheck if it is deleted by the owner , check the user of the post and id of thew logged in user  
+                Comment.deleteMany({ post: req.params.postIdToDelete }, function(err) {
+                    // if the id matches ,  first deletre all the comments 
+                    post.remove(); // and then remove the post 
+                    return res.redirect('back'); // redirect back to the home page 
+                })
+            }
+        }
+    })
 }
--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,6 @@ const router = express.Router(); // initialised router
 const postsController = require('../controllers/posts_controller');
 
 router.post('/create', passport.checkAuthentication, postsController.create);
+router.get('/destroy/:postIdToDelete', passport.checkAuthentication, postsController.destroy);
 
 module.exports = router;
