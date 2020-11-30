d0bb487aab4a146b6dcb25a40ba21577c3fbdca0
Tejas <tejasmishra2k18@gmail.com>
2020-11-30 19:01:09 +0530

    use asycn await in posts_controller.js

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -13,7 +13,21 @@ module.exports.create = function(req, res) {
     });
 }
 
-module.exports.destroy = function(req, res) {
+/* create post controller using async await
+module.exports.create = async function(req, res) {
+    try {
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
+
+        return res.redirect('back');
+    } catch (err) {
+        console.log('Error : ', err);
+    }
+} */
+
+/*module.exports.destroy = function(req, res) {
     // console.log('Request Params : ', req.params);
     // first look if the post in the database esists or not 
     Post.findById(req.params.postIdToDelete, function(err, post) {
@@ -32,4 +46,21 @@ module.exports.destroy = function(req, res) {
             }
         }
     })
+}*/
+
+// post destroy controller using async await
+module.exports.destroy = async function(req, res) {
+    try {
+        let post = await Post.findById(req.params.postIdToDelete);
+        if (post) {
+            await Comment.deleteMany({ post: req.params.postIdToDelete }, function(err) {
+                post.remove();
+                return res.redirect('back');
+            })
+        } else {
+            return res.redirect('back');
+        }
+    } catch (err) {
+        console.log('error : ', err);
+    }
 }
