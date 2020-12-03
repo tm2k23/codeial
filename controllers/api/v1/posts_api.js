6f6c8f0badce129112c14b19c53b31c7a861a95b
Tejas <tejasmishra2k18@gmail.com>
2020-12-03 16:12:22 +0530

    send all the posts from database in V1 api

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,17 @@
-module.exports.index = function(req, res) {
+const Post = require('../../../models/post');
+
+module.exports.index = async function(req, res) {
+    let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
     return res.json(200, {
         message: "List of posts V1 Api",
-        posts: []
+        posts: posts
     })
 }
