8fdd9e05791cbbba5c820d94a9845aa3f419eb6a
Tejas <tejasmishra2k18@gmail.com>
2020-12-05 23:04:32 +0530

    render HTML Template for email

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -2,11 +2,12 @@ const { readyState } = require('../config/mongoose');
 const nodeMailer = require('../config/nodemailer');
 exports.newComment = (comment) => {
     // console.log('inside new comment mailer');
+    let htmlString = nodeMailer.renderTemplate({ comment: comment }, '/comments/new_comment.ejs');
     nodeMailer.transporter.sendMail({
         from: "Codeial",
         to: comment.user.email,
         subject: "New commment",
-        html: "<h1>New comment published</h1>"
+        html: htmlString
     }, (err, info) => {
         if (err) { console.log(err); return; }
         // console.log('message sent', info);
